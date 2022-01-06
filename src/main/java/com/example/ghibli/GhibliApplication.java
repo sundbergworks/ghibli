46855f3a2f1b126e8c0987fd28e48f50d6ec211c
isaac <isaac@techvvs.io>
2022-01-06 12:48:41 -0600

    init commit

---
+++ src/main/java/com/example/ghibli/GhibliApplication.java
@@ -0,0 +1,13 @@
+package com.example.ghibli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhibliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhibliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ghibli/GhibliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ghibli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhibliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
