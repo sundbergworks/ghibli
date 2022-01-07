9cde44fb93f9dbd279a691886a816fa7b27d8d49
isaac <isaac@techvvs.io>
2022-01-06 20:09:41 -0600

    exposed endpoint to get  people data

--- src/main/java/com/example/ghibli/api/ghiblirestcontroller.java
+++ src/main/java/com/example/ghibli/api/ghiblirestcontroller.java
@@ -4,6 +4,7 @@ package com.example.ghibli.api;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -23,6 +24,34 @@ public class ghiblirestcontroller {
         return response.getBody();
 
 
+
+    }
+    @GetMapping("/getById/{id}")
+    public String getFilmById(@PathVariable("id") String filmId) {
+        RestTemplate restTemplate = new RestTemplate();
+        String fooResourceUrl = "https://ghibliapi.herokuapp.com/films/" + filmId;
+        ResponseEntity<String> response = restTemplate.getForEntity(fooResourceUrl, String.class);
+
+        return response.getBody();
+    }
+
+    @GetMapping("/getAllPeople")
+    String getAllPeople() throws JsonProcessingException {
+        RestTemplate restTemplate = new RestTemplate();
+        String fooResourceUrl = "https://ghibliapi.herokuapp.com/people";
+        ResponseEntity<String> response = restTemplate.getForEntity(fooResourceUrl, String.class);
+
+        return response.getBody();
+
+    }
+
+    @GetMapping("/getByPersonId/{id}")
+    public String getByPersonId(@PathVariable("id") String personId) {
+        RestTemplate restTemplate = new RestTemplate();
+        String fooResourceUrl = "https://ghibliapi.herokuapp.com/people/" + personId;
+        ResponseEntity<String> response = restTemplate.getForEntity(fooResourceUrl, String.class);
+
+        return response.getBody();
 
     }
 
